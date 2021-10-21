32f333bea26007dfca739e238772d8630bf339c3
Khoi Tuan <khoituan1412@gmail.com>
2021-10-21 17:56:47 +1000

    edit comments

--- server-express/controller/nasa.controller.js
+++ server-express/controller/nasa.controller.js
@@ -1,6 +1,8 @@
 const { getNASAData } = require('../service/nasa.service');
 const { downloadImage } = require('../service/downloadImage.service')
 const { getMudCracksPredictions } = require('../service/mudcracks.service');
+const { readFromS3, uploadToS3 } = require('../service/awsS3.service');
+const { readFromMongo, uploadToMongo } = require('../service/mongoDB.service');
 
 exports.getPredictions = async (req, res, next) => {
 	/**
@@ -12,15 +14,15 @@ exports.getPredictions = async (req, res, next) => {
 	 * 		download image as "nasa_id.jpg"
 	 * 		get predictions data from MongoDB
 	 * 		draw new image with predictions data
-	 * 		upload image to S3
+	 * 		upload processed image to S3
 	 * 		serve data (imgLink) from S3
 	 * else
 	 * 		download image as "nasa_id.jpg"
 	 * 		get predictions from Flask server
 	 * 		data = Flask response
 	 * 		draw new image and replace the current "nasa_id.jpg"
-	 * 		upload predictions to MongoDB and new image to S3
-	 * 		image_link = S3 response
+	 * 		upload predictions to MongoDB and processed image to S3
+	 * 		serve data (imgLink) from S3
 	 * Send image_link to client
 	 */
 	// TODO Step 1: Get input from user
@@ -30,12 +32,11 @@ exports.getPredictions = async (req, res, next) => {
 	const imageData = await getNASAData(userInput);
 	// TODO if imageDate[i]['links'].length > 1 => skip
 	const url = imageData[3]['links'][0]['href'];
-	const nasa_id = imageData[3]['data'][0]['nasa_id'];
-	console.log(url, ' ', nasa_id);
+	const nasa_id = imageData[3]['data'][0]['nasa_id'];	
+	
 
 	// TODO Step 3: Using image's meta data, download image to NASA images (local folder)
 	const imagePath = await downloadImage(url, nasa_id);
-	// console.log('Image path is ', imagePath);
 
 	// TODO Step 4: Using image, get predictions Flask server
 	const flaskResponse = await getMudCracksPredictions(imagePath);
