aa671e30d6a91b55058503c33d2d6362f94b93c1
phuongnamly <62232553+phuongnamly@users.noreply.github.com>
2021-10-28 13:10:13 +1000

    remove image-downloader module

--- server-express/service/downloadImage.service.js
+++ server-express/service/downloadImage.service.js
@@ -1,23 +1,4 @@
-const download = require('image-downloader');
 const Jimp = require('jimp');
-
-/**
- * @param {string} url the image's url
- * @param {string} nasa_id the image's nasa_id
- * @returns {string} options.dest (local) path to the downloaded image
- */
-// exports.downloadImage = async (url, nasa_id) => {
-// 	const options = {
-// 		url,
-// 		dest: `../NASA images/${nasa_id}.jpg`
-// 	}
-// 	await download.image(options)
-// 		.then(({ filename }) => {
-// 			console.log('Saved to ', filename);
-// 		})
-// 		.catch((err) => console.error(err));
-// 	return options.dest;
-// }
 
 /**
  * Save the image to the local folder.
